3feb315449b4603ee07f3d6ff3f53249a06a161c
Daniil <umarmungenfuerimmer@gmail.com>
2022-01-07 18:31:08 +0300

    Добавил оставшиеся единичные компоненты

--- mist_sema/ClientApp/src/components/Home.js
+++ mist_sema/ClientApp/src/components/Home.js
@@ -5,8 +5,12 @@ export class Home extends Component {
     static displayName = Home.name;
 
     render() {
-        return (
-            <SingleComponent endpoint={'processors'} title={"Процессор"}/>
+        return (<>
+                <SingleComponent endpoint={'processors'} title={"Процессор"}/>
+                <SingleComponent endpoint={'graphic_cards'} title={"Графический процессор"}/>
+                <SingleComponent endpoint={'system_boards'} title={"Материнская плата"}/>
+                <SingleComponent endpoint={'power_supplies'} title={"Блок питания"}/>
+            </>
         );
     }
 }
