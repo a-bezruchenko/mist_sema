977367cbb105f40a2fb6b4052f35ea57ea5d48e6
Daniil <umarmungenfuerimmer@gmail.com>
2022-01-07 19:15:11 +0300

    Добавил отображение элементов с несколькими значениями

--- mist_sema/ClientApp/src/components/Home.js
+++ mist_sema/ClientApp/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SingleComponent from "./SingleComponent";
+import MultipleComponent from "./MultipleComponent";
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -10,6 +11,8 @@ export class Home extends Component {
                 <SingleComponent endpoint={'graphic_cards'} title={"Графический процессор"}/>
                 <SingleComponent endpoint={'system_boards'} title={"Материнская плата"}/>
                 <SingleComponent endpoint={'power_supplies'} title={"Блок питания"}/>
+                <MultipleComponent endpoint={'rams'} title={"Оперативная память"}/>
+                <MultipleComponent endpoint={'storage_devices'} title={"Жёсткие диски или SSD"}/>
             </>
         );
     }
---
+++ mist_sema/ClientApp/src/components/MultipleComponent.js
@@ -0,0 +1,64 @@
+﻿import {useEffect, useState} from "react";
+import {Autocomplete, Box, Card, CardContent, CardMedia, Stack, TextField, Typography} from "@mui/material";
+import NumericInput from 'react-numeric-input';
+
+export default function MultipleComponent({title, endpoint}) {
+    let [components, setComponents] = useState([]);
+    let [allComponents, setAllComponents] = useState([]);
+    let [idToCount, setIdToCount] = useState({})
+
+    useEffect(() => {
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then((components) => components.map((component_) => ({label: component_.manufacturer + " " + component_.name, ...component_})))
+            .then((json) => {
+                console.log(json);
+                return json
+            })
+            .then((components) => setAllComponents(components))
+    }, [])
+    return (<div style={{marginTop: "20px", display: 'flex'}}>
+        <Autocomplete
+            multiple
+            disablePortal
+            options={allComponents}
+            sx={{width: 300, marginRight: 10}}
+            renderInput={(params) => <TextField {...params} label={title}/>}
+            onChange={(_, value) => {
+                setComponents(value);
+                console.log(value);
+            }}
+        />
+        <Stack>
+            {
+                components.map(component => <Card sx={{maxHeight: 151, minHeight: 151, display: 'flex'}}>
+                    <CardMedia
+                        component="img"
+                        sx={{maxWidth: 151, minWidth: 151, backgroundSize: "contain"}}
+                        image={component?.imageLink}
+                        alt={component?.label}
+                    />
+                    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                        <CardContent sx={{flex: '1 0 auto'}}>
+                            <Typography component="div" variant="h5">
+                                {component?.name}
+                            </Typography>
+                            <Typography variant="subtitle1" component="div">
+                                {component?.manufacturer}
+                            </Typography>
+                        </CardContent>
+                        <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1, justifyContent: "flex-end"}}>
+                            <NumericInput
+                                mobile
+                                min={1}
+                                value={idToCount[component?.id] ?? 1}
+                                onChange={(_, value) => idToCount[component?.id] = value}
+                            />
+                        </Box>
+                    </Box>
+                </Card>)
+            }
+        </Stack>
+
+    </div>)
+}
