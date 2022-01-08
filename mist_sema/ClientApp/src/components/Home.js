36623980139b30573bfcf45c91125e1681444851
Daniil <umarmungenfuerimmer@gmail.com>
2022-01-08 13:10:22 +0300

    Добавил валидацию

--- mist_sema/ClientApp/src/components/Home.js
+++ mist_sema/ClientApp/src/components/Home.js
@@ -1,31 +1,34 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import SingleComponent from "./SingleComponent";
 import MultipleComponent from "./MultipleComponent";
 import {Grid, Stack} from "@mui/material";
+import Validation from "./Validation";
 
-export let configurationIds = []
-
-export class Home extends Component {
-    static displayName = Home.name;
-
-    render() {
-        return (<>
-                <Grid container>
-                    <Grid item xs={"auto"}>
-                        <Stack spacing={2}>
-                            <SingleComponent endpoint={'processors'} title={"Процессор"}/>
-                            <SingleComponent endpoint={'graphic_cards'} title={"Графический процессор"}/>
-                            <SingleComponent endpoint={'system_boards'} title={"Материнская плата"}/>
-                            <SingleComponent endpoint={'power_supplies'} title={"Блок питания"}/>
-                            <MultipleComponent endpoint={'rams'} title={"Оперативная память"}/>
-                            <MultipleComponent endpoint={'storage_devices'} title={"Жёсткие диски или SSD"}/>
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Stack/>
-                    </Grid>
+export function Home() {
+    const [configIds, setConfigIds] = useState([])
+    console.log(configIds)
+    return (<>
+            <Grid container>
+                <Grid item xs={8}>
+                    <Stack spacing={2}>
+                        <SingleComponent endpoint={'processors'} title={"Процессор"} configIds={configIds}
+                                         setConfigIds={setConfigIds}/>
+                        <SingleComponent endpoint={'graphic_cards'} title={"Графический процессор"}
+                                         configIds={configIds} setConfigIds={setConfigIds}/>
+                        <SingleComponent endpoint={'system_boards'} title={"Материнская плата"} configIds={configIds}
+                                         setConfigIds={setConfigIds}/>
+                        <SingleComponent endpoint={'power_supplies'} title={"Блок питания"} configIds={configIds}
+                                         setConfigIds={setConfigIds}/>
+                        <MultipleComponent endpoint={'rams'} title={"Оперативная память"} configIds={configIds}
+                                           setConfigIds={setConfigIds}/>
+                        <MultipleComponent endpoint={'storage_devices'} title={"Жёсткие диски или SSD"}
+                                           configIds={configIds} setConfigIds={setConfigIds}/>
+                    </Stack>
                 </Grid>
-            </>
-        );
-    }
+                <Grid item xs={2}>
+                    <Validation configIds={configIds}/>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
